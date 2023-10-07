a34d19bed787638197070291f48c96fddc29d904
OLETISATISHKUMAR <satisholeti1020@gmail.com>
2023-10-08 00:32:23 +0530

    Added Login Functionallity

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      setError(null);
+
+      const response = await fetch("http://localhost:8000/api/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      console.log(data);
+      navigate("/")
+
+      if (!response.ok) {
+        setError(data.message);
+      } else if (data.success === false) {
+        setError(data.message);
+      } 
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-center text-[20px] font-bold p-3">Sign In</h1>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          className="border p-2 rounded-lg"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          className="border p-2 rounded-lg"
+          value={formData.password}
+          onChange={handleInputChange}
+        />
+        <button
+          type="submit"
+          className="border p-2 rounded-lg bg-slate-700 text-white disabled:opacity-80"
+          disabled={loading}
+        >
+          {loading ? "Loading...." : "Log in"}
+        </button>
+      </form>
+      {error && <p className="text-red-600 mt-2">{error}</p>}
+      <p className="flex gap-2 mt-2">
+        Do not have an account?
+        <Link to="/signup" className="text-blue-600">
+          {" "}
+          SignUp
+        </Link>
+      </p>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,7 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+const errorHandler = require("../utils/error")
 
 exports.create = async (req, res, next) => {
   try {
@@ -24,3 +26,22 @@ exports.create = async (req, res, next) => {
     next(error);
   }
 };
+
+
+exports.login = async(req, res, next)=>{
+  try {
+    const {email, password} = req.body
+
+    const exist = await User.findOne({ email })
+    if(!exist) return next(errorHandler(404, "User Not Found!!"))
+    const existpassword = bcrypt.compareSync(password, exist.password)
+    if(!existpassword) return next(errorHandler(401, "Wrong Credentials"))
+    const token = jwt.sign({id : exist._id}, process.env.JWT_KEY)
+  res.cookie("access_token", token, {httpOnly:true}).status(201).json({
+    exist
+  })
+
+  } catch (error) {
+    next(error)
+  }
+}
